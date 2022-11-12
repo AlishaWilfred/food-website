f38fd48adbff8fbe23a6b162e0a8014370f980be
AlishaWilfred <alisha3105henry@gmail.com>
2022-11-12 14:54:02 +0530

    first commit

---
+++ store/store.js
@@ -0,0 +1,13 @@
+import create from "zustand"
+
+export const useStore=create((set)=>({
+
+    //cart
+    cart:{
+        pizzas:[]
+    },
+   addPizza:(data)=>set((state)=>({cart:{pizzas:[...state.cart.pizzas,data]}})),      //add to cart
+   removePizza:(index)=>set((state)=>({cart:{pizzas:state.cart.pizzas.filter((_,i)=>i!=index)}})),
+   resetCart:()=>set(()=>({cart:{pizzas:[]}}))
+})
+)
